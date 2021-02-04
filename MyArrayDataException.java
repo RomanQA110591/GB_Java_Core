08daaeade9bd1acb94100693ffbf459245bce456
RomanQA110591 <roman.sapachev@mail.ru>
2021-02-04 19:41:54 +0300

    Проект

---
+++ MyArrayDataException.java
@@ -0,0 +1,13 @@
+package ru.GB.Lesson2;
+
+public class MyArrayDataException extends Exception {
+    public int i;
+    public int j;
+
+    MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+
+    }
+
+}
