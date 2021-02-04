175299cec345eac7ce64d3e638d50cbd00994ea3
RomanQA110591 <roman.sapachev@mail.ru>
2021-02-04 19:41:54 +0300

    Проект

---
+++ MyArraySizeException.java
@@ -0,0 +1,11 @@
+package ru.GB.Lesson2;
+
+public class MyArraySizeException extends Exception {
+    MyArraySizeException(String msg) {
+      super(msg);
+   }
+
+    public MyArraySizeException() {
+
+    }
+}
